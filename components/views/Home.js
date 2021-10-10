d623d22e94bfcb10692b9068bfa2bdeb0c341876
CGrisham1 <89211813+CGrisham1@users.noreply.github.com>
2021-10-10 12:08:04 -0500

    pushing working API to master

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,1 +1,7 @@
-export default () => ``
+const kelvinToFahrenheit = kelvinTemp =>
+    Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+
+export default (st) => `<h3>
+Weather in ${st.weather.city} ${kelvinToFahrenheit(st.weather.temp)}F, feels
+like ${kelvinToFahrenheit(st.weather.feelsLike)}F
+</h3>`;
