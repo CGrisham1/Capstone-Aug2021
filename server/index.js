a6b1d08901cf90acaf3942ab218a89da1d825b6e
CGrisham1 <89211813+CGrisham1@users.noreply.github.com>
2021-10-14 18:52:33 -0500

    added middleware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const http = require("http");
 const server = http.createServer(() => {});
 
+const logging = (request, response, next) => {
+    console.log(`${request.method} ${request.url} ${Date.now()}`);
+    next();
+  };
+
+  app.use(logging);
+
 if (request.url === "/status" && request.method === "GET") {
   response.writeHead(200, { "Content-Type": "application/json" });
   response.write(JSON.stringify({ message: "Service healthy" }));
   response.end();
 }
+
 
 server.listen(4040);
 console.log("Listening on port 4040");
