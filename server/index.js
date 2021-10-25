f292412ac251a8ed8c15459495bc8a2370646f9c
CGrisham1 <89211813+CGrisham1@users.noreply.github.com>
2021-10-25 14:51:39 -0500

    troubleshooting route in server folder

--- server/index.js
+++ server/index.js
@@ -54,6 +54,13 @@ app
     response.json({requestBody: request.body});
   });
 
+  app.route("/users/:id").get((request, response) => {
+    // express adds a "params" Object to requests
+    const id = request.params.id;
+    // handle GET request for post with an id of "id"
+    response.send(JSON.stringify({ user_id: id }));
+  });
+
 const PORT = process.env.PORT || 4040;
   app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
