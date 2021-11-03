f48fe12e4c316a6c9809f002ac1320b6aa460d97
CGrisham1 <89211813+CGrisham1@users.noreply.github.com>
2021-11-02 21:04:03 -0500

    routes posts.js updates

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,7 +7,7 @@ router.post('/', (request, response) => {
     const newPost = new post.model(request.body);
     newPost.save((err, post) => {
 
-      return err ? response.sendStatus(500).json(err) : response.json(pizza);
+      return err ? response.sendStatus(500).json(err) : response.json(post);
     });
   });
 
@@ -15,7 +15,11 @@ router.post('/', (request, response) => {
 // Delete a post by ID
 router.delete('/:id', (request, response) => {
     post.model.findByIdAndRemove(request.params.id, {}, (error, data) => {
+
       if (error) return response.sendStatus(500).json(error);
       return response.json(data);
     });
   });
+
+
+module.exports = router;
