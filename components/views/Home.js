82b73d0d3211961870c9524368cc6419dd1f3ef4
CGrisham1 <89211813+CGrisham1@users.noreply.github.com>
2021-11-09 16:43:38 -0600

    HTML and CSS styling

--- components/views/Home.js
+++ components/views/Home.js
@@ -4,12 +4,16 @@ const kelvinToFahrenheit = kelvinTemp =>
 
 
 
-export default (st) => `<h3>
+export default (st) => `<h3 id="weatherapi">
 Weather in Springfield is ${kelvinToFahrenheit(st.weather.temp)}F, Feels
 like ${kelvinToFahrenheit(st.weather.feelsLike)}F
 </h3>
+<div>
+<h3 id="homeintro"> Hi! My name is Clay and I am a recovering alcoholic. I am 24 years old, that may sound young but age does not have a restriction for addiction. I am typing this today confidently telling the world that I am 14 months sober from alcohol and you can be too. The road is long and very tough but with a stable support system or simply some resources, you too can achieve the goal of sobriety like I have.</h3>
 
-Hi! My name is Clay Grisham and I am a recovering alcoholic. I am 24 years old, that may sound young but age does not have a restriction for addiction. I am typing this today confidently telling the world that I am 14 months sober from alcohol and you can be too. The road is long and very tough but with a stable support system or simply some resources, you too can achieve the goal of sobriety like I have.
+
+<h3 id="homeintro2"> Within this website, I have created a platform that will enable others struggling with alcoholism or alcohol abuse to share their story or simply find some resources.</h3></div> 
+
 <div id=opening>
 <p>Together we can break the stigma</p>
 </div>
--- components/views/Resources.js
+++ components/views/Resources.js
@@ -2,7 +2,7 @@ export default (st) => `
 
 
 
-<p id="rp1">Below are some helpful links that are specific to Alcohol abuse and recovery</p>
+<h3 id="rp1">Below are some helpful links that are specific to Alcohol abuse and recovery</h3>
 
 <div class="reslinks"
     <ul>
@@ -10,8 +10,14 @@ export default (st) => `
         <li><a href="https://www.recovery.org/">Addiction Recovery - Alcohol Specific</a></li>
         <li><a href="https://www.alcoholrehabguide.org/resources/">Alcohol Rehab Guide</a></li>
         <li><a href="https://www.samhsa.gov/find-help/national-helpline">SAMHSA National Helpline</a></li>
+        <li><a href="https://womenforsobriety.org/">Women for Sobriety</a></li>
+        <li><a href="https://alcoholtreatment.niaaa.nih.gov/">Alcohol Treatment Finder</a></li>
+        <li><a href="https://www.niaaa.nih.gov/">National Institute on Alcohol Abuse and Recovery</a></li>
     </ul>
         </div>
+
+
+        <img src="https://images.unsplash.com/photo-1520732770642-f4d947ca9653?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGVtcHR5JTIwYm90dGxlc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" id="emptybottle">
 
 
 
